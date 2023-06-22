0d3e7d40be25fadf30a1ef60960a68eed809e677
unknown <Sena@MEDAPRCSGFSD829.FormacionSena.RED>
2023-06-22 13:14:31 -0500

    Controlador de cliente Finalizado

---
+++ controllers/clienteController.js
@@ -0,0 +1,14 @@
+const {ClienteModels} = require("../models/ClienteModel");
+
+const consultar = async (req, res) => {
+    try {
+        const clientes = await ClienteModels.findAll();
+
+    res.status(200).json(clientes);
+    } catch (error) {
+        res.status(500).json({error: "No se pudo haser la consulta a la tabla clientes"}); 
+    }
+    
+}
+
+module.exports = consultar;
