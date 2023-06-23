e79d2745f40cedef2e0386bd49de22019dd0d256
Jesús CF <jesus3928cf@gmail.com>
2023-06-22 20:46:22 -0500

    solución de conflictos

--- routes/proveedorRoutes.js
+++ routes/proveedorRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const consu = require('../controllers/proveedorController');
+const {consu} = require('../controllers/proveedorController');
 const router = express.Router();
 
 /// peticiones para Proveedor
