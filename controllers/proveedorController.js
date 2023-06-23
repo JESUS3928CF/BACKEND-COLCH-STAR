aa2936f52c669a0677397d6c3368a364ef43a735
Brian-3112 <pepito123@gmail.com>
2023-06-22 20:34:47 -0500

    szs

--- controllers/proveedorController.js
+++ controllers/proveedorController.js
@@ -4,14 +4,16 @@ const { ProveedorModels } = require("../models/ProveedorModel");
 
 const consu = async (req, res) => {
     try {
+        //por el meto findAll se buscna todos los registros
         const resultado = await ProveedorModels.findAll();
-
+        
     res.status(200).json(resultado)
     } catch (e) {
-        res.status(500).json({e:"no se consulto la tabla"})
+        console.e("error en la busqueda de la tabla ",e);
+        res.status(500).json({e:"no se consulto la tabla"});
     } 
       
 }
 
-module.exports = consu;
+module.exports = {consu};
 
