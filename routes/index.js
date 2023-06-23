0bd145ba728c9f529a088feee453d15bd2aa1031
LaNoviaDeHeeseung <132948968+LaNoviaDeHeeseung@users.noreply.github.com>
2023-06-23 14:03:40 -0500

    error en usuario

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routerCliente = require('./clienteRoutes');
 const routerProveedor = require('./proveedorRoutes');
+const routerUsuario = require('./usuarioRoutes');
 
 function routerApi(app){
     const router = express.Router();
     app.use("/api", router)
     router.use('/clientes', routerCliente);
-    router.use('/proveedor', routerProveedor)
+    router.use('/proveedor', routerProveedor);
+    router.use('/usuario', routerUsuario)
 
 }
 
---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {consultar, agregar, actualizar} = require('../controllers/usuarioController');
+const router = express.Router();
+
+/// peticiones para clientes
+
+//* Find all
+router.get("/", consultar );
+
+//* Insert One
+router.post("/", agregar );
+
+
+router.patch('/:id', actualizar);
+
+
+
+module.exports = router;
