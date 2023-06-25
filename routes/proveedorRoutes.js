749e358ac0eff5e5198f6aa23a38345201a78722
Brian-3112 <pepito123@gmail.com>
2023-06-24 23:47:14 -0500

    holaa

--- routes/proveedorRoutes.js
+++ routes/proveedorRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const {consu, agregar, actualizar, cambiarEstado} = require('../controllers/proveedorController');
 const router = express.Router();
 
-/// peticiones para Proveedor
-router.get("/", consu );
 
+//* Find all
+router.get("/", consu );
 
 //* Insert One
 router.post("/", agregar );
