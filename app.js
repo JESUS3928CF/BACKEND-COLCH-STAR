8f5e738f4c3bc1fd9a291ea34d43b53d942038ba
Brian-3112 <pepito123@gmail.com>
2023-07-16 18:30:52 -0500

    sz

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const db = require("./config/db");
-// const clienteRoutes = require('./routes/clienteRoutes');
-// const proveedorRoutes = require('./routes/proveedorRoutes');
 const cors = require('cors');
 const dotenv = require('dotenv/config');
 const routerApi = require("./routes/index");
@@ -24,7 +22,7 @@ port = process.env.PORT || 4000;
 ///Middlewares
 app.use(cors());
 
-// app.use("/api/clientes", clienteRoutes);
+
 routerApi(app);
 
 app.get("/api", (req, res) => {
