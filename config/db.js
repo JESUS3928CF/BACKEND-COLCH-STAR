5111bc8e44226962169994348d02fa179fb5098c
Jesús Cochero <jesus3928cf@gmail.com>
2024-01-26 15:25:16 -0500

    ajustes en la  configuración para la conexión en a la DB

--- config/db.js
+++ config/db.js
@@ -19,14 +19,37 @@ const dbLocal = new Sequelize(
             acquire: 30000,
             idle: 10000,
         },
+        operatorAliases: false
+    }
+);
+
+const dbRemote = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        define: {
+            timestamps: false,
+        },
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
         operatorAliases: false,
-        // dialectOptions: {
-        //     ssl: {
-        //         rejectUnauthorized: true,
-        //     },
-        // },
+
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
     }
 );
 
 // Exporta el objeto db que desees usar en tu aplicación
-module.exports =  dbLocal;
+module.exports = process.env.USE_REMOTE_DB == "true" ? dbRemote : dbLocal;
+
