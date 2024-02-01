86c993abcbee7ad0f98c792e7fa3e5bba4caa13d
Jesús CF <jesus392864cf@gmail.com>
2024-02-01 17:17:55 -0500

    Trabajando en las fechas

--- helpers/formatearDatos.js
+++ helpers/formatearDatos.js
@@ -1,17 +1,12 @@
 const formatDate = date => {
-    const newDate = new Date(date);
-    return new Intl.DateTimeFormat('es', { dateStyle: 'medium' }).format(
-        newDate
-    );
+
+    return date;
+    
 }
 
 const formatMoney = (value) => {
-    const formatter = new Intl.NumberFormat("es-US", {
-        style: 'currency',
-        currency: 'USD'
-    })
 
-    return formatter.format(value)
+    return value;
 }
 
 module.exports = {formatDate, formatMoney};
